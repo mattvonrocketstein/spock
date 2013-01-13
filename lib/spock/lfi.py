f0d1b0533cb8274223582a73948890db9771b60a
matt <mvr@laptop>
2013-01-13 12:50:34 -0500

    lfi

---
+++ lib/spock/lfi.py
@@ -0,0 +1,80 @@
+""" spock.lfi
+
+    Implementing logics for formal inconsistency.
+
+    http://sqig.math.ist.utl.pt/pub/MarcosJ/01-CM-tableaux.pdf
+"""
+
+class Operator(object):
+    def __init__(self, table):
+        self.table = {}
+        for args, result in table:
+            self.table[tuple(args)]=result
+
+    @property
+    def arity(self):
+        return len(self.table[0][0])
+
+    def __call__(self, *args):
+        return self.table[args]
+
+class CommutingOperator(Operator):
+    def __call__(self, *args):
+        try:
+            return super(CommutingOperator,self).__call__(*args)
+        except KeyError:
+            args = list(args)
+            args.reverse()
+            return super(CommutingOperator,self).__call__(*args)
+
+class Logic(object):
+    def __init__(self, operators):
+        self.operators = operators
+
+negative_by_default = Operator([
+    [['t'],'f'],
+    [['i'],'f'],
+    [['f'],'t'],
+    [['u'],'u'],
+    ])
+
+_and = CommutingOperator([
+    [['t', 't'], 't'],
+    [['t', 'i'], 'i'],
+    [['t', 'f'], 'f'],
+    [['i', 'i'], 'i'],
+    [['u', 'u'], 'u'],
+    [['u', 't'], 'u'],
+    [['u', 'i'], 'u'],
+    [['u', 'f'], 'f'],
+    [['f', 'f'], 'f'],
+    [['f', 'i'], 'f'], ])
+
+_arrow = Operator([
+    [ ['t','t'], 't'],
+    [ ['t','i'], 'f'],
+    [ ['t','u'], 'f'],
+    [ ['t','f'], 'f'],
+    [ ['i','t'], 'i'],
+    [ ['i','i'], 'i'],
+    [ ['i','u'], 'f'],
+    [ ['i','f'], 'f'],
+    [ ['u','t'], 't'],
+    [ ['u','i'], 't'],
+    [ ['u','u'], 't'],
+    [ ['u','f'], 'f'],
+    [ ['f','t'], 't'],
+    [ ['f','i'], 't'],
+    [ ['f','u'], 't'],
+    [ ['f','f'], 't'],
+    ])
+_or = CommutingOperator([
+    [['t', 't'], 't'],
+    [['t', 'u'], 't'],
+    [['i', 'i'], 'i'],
+    [['u', 'i'], 'i'],
+    [['u', 'f'], 'u'],
+    [['t', 'f'], 't'],
+    [['t', 'i'], 't'],
+    [['f', 'f'], 'f'],
+    [['f', 'i'], 'i'], ])
