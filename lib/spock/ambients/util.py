2face22f0586a587568ee2bfb7338a81acf71323
matt@laptop <matt@laptop>
2013-11-02 17:31:55 -0400

    tests for ambient_args() decorator

--- lib/spock/ambients/util.py
+++ lib/spock/ambients/util.py
@@ -1,19 +1,8 @@
 """ spock.ambients.util
 """
+from goulash.decorators import arg_types
 
-class arg_types(object):
-    """ A decorator which enforces the rule that all arguments must be
-        of type "Ambient".  All keyword arguments are ignored.
-    """
-    def __init__(self, fxn, types):
-        self.fxn = fxn
-        self.types = types if isinstance(types, (list, tuple)) else [types]
+from spock.ambients.abstract import AbstractAmbient
 
-    def __call__(self, *args, **kargs):
-        assert all(
-            [ any([ isinstance(a, x) for x in self.types]) for a in args ] )
-        return self.fxn(*args, **kargs)
-
-from .abstract import AbstractAmbient
-
-ambient_args = lambda fxn: arg_types(fxn, [AbstractAmbient])
+ambient_args = lambda fxn: arg_types(AbstractAmbient)(fxn)
+ambient_args.ArgTypeError = arg_types.ArgTypeError
--- lib/spock/tests/test_ambients.py
+++ lib/spock/tests/test_ambients.py
@@ -1,9 +1,23 @@
 """ spock.tests.test_ambients
 """
 import unittest2 as unittest
-from spock.ambients  import Ambient
+
+from spock.ambients  import Ambient, ambient_args
+
+class TestTestDecorators(unittest.TestCase):
+    def setUp(self):
+        self.ambient = Ambient()
+
+    def test_with_ambients(self):
+        tmp = ambient_args(lambda x: x)
+        tmp(self.ambient)
+
+    def test_with_non_ambients(self):
+        tmp = ambient_args(lambda x: x)
+        self.assertRaises(ambient_args.ArgTypeError, lambda: tmp(1))
 
 class TestAmbient(unittest.TestCase):
+
     def test_init(self):
         Ambient()
         Ambient(parent=None)
